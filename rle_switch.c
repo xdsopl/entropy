ea4a68989668bef7f081e662ecadcfb50a664393
Ahmet Inan <xdsopl@gmail.com>
2025-01-18 16:21:29 +0100

    added switching variant of vli based rle

---
+++ rle_switch.c
@@ -0,0 +1,108 @@
+/*
+Variable length integer based run length encoding that switches between zeros and ones
+
+Copyright 2025 Ahmet Inan <xdsopl@gmail.com>
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "common.h"
+
+int putval(int val) {
+	static int order;
+	while (val >= 1 << order) {
+		if (putbit(0))
+			return -1;
+		val -= 1 << order;
+		order += 1;
+	}
+	if (putbit(1))
+		return -1;
+	if (write_bits(val, order))
+		return -1;
+	order -= 1;
+	if (order < 0)
+		order = 0;
+	return 0;
+}
+
+int getval() {
+	static int order;
+	int val, sum = 0, ret;
+	while ((ret = getbit()) == 0) {
+		sum += 1 << order;
+		order += 1;
+	}
+	if (ret < 0)
+		return -1;
+	if (read_bits(&val, order))
+		return -1;
+	order -= 1;
+	if (order < 0)
+		order = 0;
+	return val + sum;
+}
+
+int putrle(int bit) {
+	static int prv = 2, cnt;
+	if (cnt < 0 || prv < 0)
+		return -1;
+	if (bit < 0)
+		return cnt = putval(cnt);
+	if (prv > 1)
+		return putbit(prv = bit);
+	if (bit != prv) {
+		prv = bit;
+		return cnt = putval(cnt);
+	}
+	cnt++;
+	return 0;
+}
+
+int getrle() {
+	static int bit = 2, cnt;
+	if (cnt < 0 || bit < 0)
+		return -1;
+	if (bit > 1) {
+		bit = getbit();
+		if (bit < 0)
+			return -1;
+		bit = !bit;
+	}
+	if (!cnt) {
+		bit = !bit;
+		cnt = getval();
+		if (cnt < 0)
+			return -1;
+	} else {
+		--cnt;
+	}
+	return bit;
+}
+
+int main(int argc, char **argv) {
+	if (argc != 2)
+		return 1;
+	if (*argv[1] != 'e' && *argv[1] != 'd')
+		return 1;
+	int bytes = getleb128();
+	if (bytes <= 0)
+		return 1;
+	putleb128(bytes);
+	if (*argv[1] == 'e') {
+		fprintf(stderr, "%s: run length encoding %d bytes\n", argv[0], bytes);
+		for (int bits = 8 * bytes; bits; --bits)
+			putrle(getbit());
+		putrle(-1); // flush
+		flush_bits();
+	} else {
+		fprintf(stderr, "%s: run length decoding %d bytes\n", argv[0], bytes);
+		for (int bits = 8 * bytes; bits; --bits)
+			putbit(getrle());
+	}
+	double change = 100.0 * (wrote_bytes - read_bytes) / read_bytes;
+	fprintf(stderr, "%s: read %d and wrote %d bytes %+.2f%%\n", argv[0], read_bytes, wrote_bytes, change);
+	return 0;
+}
+
