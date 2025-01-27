a2d9cd42df6f62e847c3727cd0a4f285029d7af4
Ahmet Inan <xdsopl@gmail.com>
2025-01-27 08:59:57 +0100

    renamed for clarity

--- rle_switch.c
+++ rle_switch.c
@@ -9,38 +9,38 @@ Copyright 2025 Ahmet Inan <xdsopl@gmail.com>
 #include <stdint.h>
 #include "common.h"
 
-int putval(int val, int bit) {
+int putval(int val, int ctx) {
 	static int order[2];
-	while (val >= 1 << order[bit]) {
+	while (val >= 1 << order[ctx]) {
 		if (putbit(0))
 			return -1;
-		val -= 1 << order[bit];
-		order[bit] += 1;
+		val -= 1 << order[ctx];
+		order[ctx] += 1;
 	}
 	if (putbit(1))
 		return -1;
-	if (write_bits(val, order[bit]))
+	if (write_bits(val, order[ctx]))
 		return -1;
-	order[bit] -= 1;
-	if (order[bit] < 0)
-		order[bit] = 0;
+	order[ctx] -= 1;
+	if (order[ctx] < 0)
+		order[ctx] = 0;
 	return 0;
 }
 
-int getval(int bit) {
+int getval(int ctx) {
 	static int order[2];
 	int val, sum = 0, ret;
 	while ((ret = getbit()) == 0) {
-		sum += 1 << order[bit];
-		order[bit] += 1;
+		sum += 1 << order[ctx];
+		order[ctx] += 1;
 	}
 	if (ret < 0)
 		return -1;
-	if (read_bits(&val, order[bit]))
+	if (read_bits(&val, order[ctx]))
 		return -1;
-	order[bit] -= 1;
-	if (order[bit] < 0)
-		order[bit] = 0;
+	order[ctx] -= 1;
+	if (order[ctx] < 0)
+		order[ctx] = 0;
 	return val + sum;
 }
 
